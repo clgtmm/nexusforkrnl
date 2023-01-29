d3c2d762f7ebfe2d086937d09d8773e2e4d8f427
clgtmm <103909534+clgtmm@users.noreply.github.com>
2023-01-29 19:10:04 +0700

    Create nexus.lua

---
+++ nexus.lua
@@ -0,0 +1,43 @@
+repeat wait()
+until game:IsLoaded()
+pcall(function()
+	Nexus_Version = 101
+	loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua')()
+	Nexus:Connect()
+end)
+wait(0.1)
+pcall(function()
+	Nexus_Version = 101
+	loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua')()
+	Nexus:Connect()
+end)
+wait(0.1)
+pcall(function()
+	Nexus_Version = 101
+	loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua')()
+	Nexus:Connect()
+end)
+wait(0.1)
+pcall(function()
+	Nexus_Version = 101
+	loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua')()
+	Nexus:Connect()
+end)
+wait(0.1)
+pcall(function()
+	Nexus_Version = 101
+	loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua')()
+	Nexus:Connect()
+end)
+wait(5)
+pcall(function()
+	Nexus_Version = 101
+	loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua')()
+	Nexus:Connect()
+end)
+wait(10)
+pcall(function()
+	Nexus_Version = 101
+	loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua')()
+	Nexus:Connect()
+end)
